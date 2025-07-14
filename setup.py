5c946133715c16deeb1183ed6b1ca40a74f02c32
Arnau Garcia Mesa <arnau.garcia@bsc.es>
2025-07-14 12:39:48 +0200

    updated readme

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name='QuantifyDrivers', 
     version='0.1.0',         
-    url='Implementation of a methodology using explainable ML to quantify drivers of temperature extremes ona daily basis',
-    description="A description of what QuantifyDrivers does.",
+    url='https://earth.bsc.es/gitlab/agarci8/quantifydrivershw.git',
+    description="Implementation of a methodology using explainable ML to quantify drivers of temperature extremes ona daily basis.",
     install_requires=[],
     packages=find_packages(),
     long_description=open('README.md').read(),
